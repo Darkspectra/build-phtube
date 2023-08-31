afecd3e2c645fbd51e33a52488cb2a11c2f35346
Darkspectra <ftanvirnovo@gmail.com>
2023-09-01 03:59:45 +0600

    part-1

--- javascript/tube.js
+++ javascript/tube.js
@@ -5,6 +5,22 @@ function home(){
     window.location.href = 'http://127.0.0.1:5500/index.html';
 }
 
+// ==============================
+const loadData = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/videos/categories')
+    const data = await res.json();
+    const video = data.data;
+    console.log(video[0].category_id);
+}
+
+loadData();
+
+
+
+// =============================
+
+
+
 // function submit_answer(){
 //     const answer = document.getElementById('answer');
 //     const display = document.getElementById('show_answer');
