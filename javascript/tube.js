d3ed5f5894d9b01613f74adec9da422e931156a7
Darkspectra <ftanvirnovo@gmail.com>
2023-09-01 03:42:21 +0600

    blog

---
+++ javascript/tube.js
@@ -0,0 +1,16 @@
+function new_page(){
+    window.location.href = 'http://127.0.0.1:5500/blog.html';
+}
+function home(){
+    window.location.href = 'http://127.0.0.1:5500/index.html';
+}
+
+// function submit_answer(){
+//     const answer = document.getElementById('answer');
+//     const display = document.getElementById('show_answer');
+//     const p = document.createElement('p');
+//     p.innerText = answer.value;
+//     display.appendChild(p);
+//     answer.value = "";   
+// }
+
