68a0ada4f59e915a7e8d2ca0291707357a28d2b7
Darkspectra <ftanvirnovo@gmail.com>
2023-09-01 22:01:28 +0600

    part-40

--- javascript/tube.js
+++ javascript/tube.js
@@ -5,20 +5,41 @@ function home() {
     window.location.href = 'http://127.0.0.1:5500/index.html';
 }
 
+
+
 // ==============================
-const loadData = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/videos/category/1000')
+const loadData = async (id) => {
+    const res = await fetch("https://openapi.programming-hero.com/api/videos/category/1000")
     const data = await res.json();
     const video = data.data;
     // console.log(video[0].thumbnail);
-    displayVideos(video);
+    if (video.length===0){
+        displayNoVideo();
+    }
+    else{
+        displayVideos(video);
+    }
+}
+const loadData2 = async (id) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
+    const data = await res.json();
+    const video = data.data;
+    if (video.length===0){
+        displayNoVideo();
+    }
+    else{
+        displayVideos(video);
+    }
 }
 
 const displayVideos = (videos) => {
     const videoContainer = document.getElementById('video-container');
+    const noVideoContainer = document.getElementById('no_display');
+    noVideoContainer.textContent = '';
+
+    videoContainer.textContent = '';
 
     videos.forEach(video => {
-        console.log(video);
         const videoCard = document.createElement('div');
         videoCard.classList = `card bg-base-100 p-4 shadow-xl`;
         if (video.authors[0].verified){
@@ -64,6 +85,48 @@ const displayVideos = (videos) => {
         }
     });
 }
+
+const displayNoVideo = () => {
+    const noVideoContainer = document.getElementById('no_display');
+    const videoContainer = document.getElementById('video-container');
+    videoContainer.textContent = '';
+    noVideoContainer.textContent = '';
+
+    const noVideoCard = document.createElement('div');
+    // videoCard.classList = ``;
+    noVideoCard.innerHTML = `
+        <p class="ml-10 "><img src="images/Icon.png"></p>
+    `;
+    noVideoContainer.appendChild(noVideoCard);
+
+}
+
+
+const all_video = async () => {
+    const res = await fetch("https://openapi.programming-hero.com/api/videos/categories")
+    const data = await res.json();
+    const category = data.data;
+    loadData2(category[0].category_id);
+}
+const music = async () => {
+    const res = await fetch("https://openapi.programming-hero.com/api/videos/categories")
+    const data = await res.json();
+    const category = data.data;
+    loadData2(category[1].category_id);
+}
+const comedy = async () => {
+    const res = await fetch("https://openapi.programming-hero.com/api/videos/categories")
+    const data = await res.json();
+    const category = data.data;
+    loadData2(category[2].category_id);
+}
+const drawing = async () => {
+    const res = await fetch("https://openapi.programming-hero.com/api/videos/categories")
+    const data = await res.json();
+    const category = data.data;
+    loadData2(category[3].category_id);
+}
+
 
 loadData();
 
